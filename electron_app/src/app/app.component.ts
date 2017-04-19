59ff788936de2fbb67074f5e1cd9c7991a2addc8
Robert Banziziki <robert.banziziki@cluepoints.com>
2017-04-19 14:28:51 +0200

    [ Ticket: X] Subject: Description:

---
+++ electron_app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'WatchTower';
+  description = 'Developer tools for managing micro services in development processes';
+}
