bf0b3653a25370cd60071c328b2f24ca4348fd7e
Robert Banziziki <robert.banziziki@cluepoints.com>
2017-04-20 12:41:26 +0200

    Changed to angular material successfully

--- electron_app/src/app/app.module.ts
+++ electron_app/src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { ContentComponent } from './content/content.component';
 import { ServiceCardComponent } from './service-card/service-card.component';
-import { MaterializeModule } from "ng2-materialize";
 import { ServiceLiveComponent } from './service-live/service-live.component';
-import { ServiceDashboardComponent } from './service-dashboard/service-dashboard.component'
+import { ServiceDashboardComponent } from './service-dashboard/service-dashboard.component';
+
 
 @NgModule({
   declarations: [
@@ -24,7 +26,8 @@ import { ServiceDashboardComponent } from './service-dashboard/service-dashboard
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterializeModule.forRoot(),
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
